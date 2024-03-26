edcffb6cc515f9f4e160c1e7882f1a4a79a526af
Lê Quang Huy <lequanghuy2108@gmail.com>
2024-03-26 20:23:02 +0700

    Add second paragraph in About-Us component - Build onClick function for 2 arrow icon - Add 2 dots underneath the paragraph

--- front-end/src/Components/AboutUs/AboutUs.jsx
+++ front-end/src/Components/AboutUs/AboutUs.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './AboutUs.css'
 import * as Icon from 'react-feather';
 
 export const AboutUs = () => {
+
+  const [state,setState] = useState(1);
+
+  const handleToglleUp = () => {
+    if(state < 2) {
+      setState(state + 1)
+    }
+  }
+  const handleToggleDown = () => {
+    if(state > 1) {
+      setState(state - 1)
+    }
+  }
+
+  console.log(state)
+
   return (
     <div className='container' id='about-us'>
         <div className="aboutUs">
             <div className="aboutUs-content">
               <h1>ABOUT US</h1>
-              <Icon.ChevronLeft size={48} id='chevronLeft'/>
-              <p>
+              <Icon.ChevronLeft size={48} onClick={handleToggleDown} className='icon' id='chevronLeft'/>
+              <p className={state === 1 ? 'show' : 'remove'}>
                 We welcome you to a dedicated platform crafted to 
                 streamline IT resources and books, tailored specifically 
                 for IT students. Our mission is simple: to make self-learning 
@@ -17,7 +33,18 @@ export const AboutUs = () => {
                 enriching. Join us and embark on a journey where knowledge meets 
                 empowerment, one resource at a time.
               </p>
-              <Icon.ChevronRight size={48} id='chevronRight'/>
+              <p className={state === 2 ? 'show' : 'remove'}>
+              We provide trustworthy sources in IT, empowering you to learn 
+              efficiently and fearlessly. Count on our dedication to deliver fresh 
+              insights daily, keeping you updated on the latest trends without 
+              worrying about fake information. With us, stay ahead in the ever-evolving 
+              world of technology.
+              </p>
+              <Icon.ChevronRight onClick={handleToglleUp} className='icon' size={48} id='chevronRight'/>
+              <div className="dots">
+                <span className={state === 1 ? 'activeDot' : ''} ></span>
+                <span className={state === 2 ? 'activeDot' : ''} ></span>
+              </div>
             </div>
         </div>
     </div>
